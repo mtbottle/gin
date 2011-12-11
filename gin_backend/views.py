dc6436dec42738496e0c8462ef1b5827f1dee862
Michelle Tran <mtsyne@gmail.com>
2011-12-11 17:53:54 -0500

    Added post_add_new_group

--- gin_backend/views.py
+++ gin_backend/views.py
@@ -14,26 +14,43 @@ import controllers
 def index(request):
 	return render_to_response("main.html")
 
-def get_all_groups(request):
-	''' Return Http request of json with all groups data. '''
-	# get all the group data from database and serialize it into json
-	groups = controllers.get_all_groups()
-	json_data = serializers.serialize("json", groups)
+def post_add_new_group(request):
+	''' Given a post structured as : {'name':__, 'location': __, 'description': __}
+		Create a new group and save it. Return the new group id in response. '''
+	try:
+		name = request.REQUEST['name']
+		location = request.REQUEST['location']
+		description = request.REQUEST['description']
 
-	return HttpResponse(json_data, mimtype="application/son")
+	# if any of the request items are missing, return HttpResponse of error
+	except KeyError as e:
+		error = {'msg': 'Missing parameters: %s' % e.message}
+		return HttpResponse(json.dumps(error), mimetype = 'application/json', status = 400)
 
-def get_all_gips(request):
-	''' Return Http request of json with all gip data. '''
-	# get all the group data from database and serialize it into json
-	gips = controllers.get_all_gips()
-	json_data = serializers.serialize("json", gips)
+	# create the database object and return id
+	group_id = create_group(name, location, description)
+	response = {'id': group_id}
+	return HttpResponse(json.dumps(response), mimetype = 'application/json', status = 200)
 
-	return HttpResponse(json_data, mimtype="application/son")
+#def get_all_groups(request):
+#	''' Return Http request of json with all groups data. '''
+#	# get all the group data from database and serialize it into json
+#	groups = controllers.get_all_groups()
+#	json_data = serializers.serialize("json", groups)
+
+#	return HttpResponse(json_data, mimtype="application/json")
+
+#def get_all_gips(request):
+#	''' Return Http request of json with all gip data. '''
+#	# get all the group data from database and serialize it into json
+#	gips = controllers.get_all_gips()
+#	json_data = serializers.serialize("json", gips)
+
+#	return HttpResponse(json_data, mimtype="application/json")
 
 def message(request):
 	message = Message.objects.all()
 	group = Group.objects.all()
 	return render_to_response('message.html',{"message":message,"group":group})
-
 
 
