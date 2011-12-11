5d97b1a9da1897f6b6d706b8057d9c6b4fd20795
f <f@fmbp.(none)>
2011-12-11 12:10:28 -0800

    took out some hardcoded references to external websites, merged message and tag functions

---
+++ frontend/global.js
@@ -0,0 +1,14 @@
+﻿url = ""
+
+function load_javascript_file(src_) {
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = url + "/" + src_;
+    document.getElementsByTagName("head")[0].appendChild(s);
+}
+function load_css_file(src_) {
+    var s = document.createElement("link");
+    s.rel = "stylesheet";
+    s.href = url + "/" + src_;
+    document.getElementsByTagName("head")[0].appendChild(s); 
+}
